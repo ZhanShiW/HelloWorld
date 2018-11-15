a1122c1b4f30b1855c1bf9e3a2fc09a66a176ad7
ZhanShiW <412099720@qq.com>
2018-11-15 18:00:05 +0800

    Original Version

---
+++ HelloWorld.java
@@ -0,0 +1,81 @@
+package my;
+
+//import java.util.Random;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+//		int[] re = new int[3];
+//		int count = 0;	
+//		
+//		Random a = new Random();		
+//		while(count < 3) {
+//			int s = a.nextInt(95);
+//			
+//				boolean exist = false;
+//				for (int i =0; i < count; i++) {
+//					if (re[i] == s) {
+//						exist = true;
+//						break;
+//				}
+//			}
+//				if (exist) {
+//					continue;
+//				}
+//				else {
+//					re[count]  = s;
+//					count++;
+//				}		
+//		}
+//		
+//		for(int i = 0; i < re.length ; i++) {
+//			System.out.println("random number is " + re[i]);
+//		}
+//		Monkey m1 = new Monkey(101, "round");
+//		Monkey m2 = new Monkey(102, "rect");
+//		Monkey m3 = new Monkey(103, "tri");
+//		Monkey m4 = new Monkey(104, "elic");
+//		m1.next = m2;
+//		m2.next = m3;
+//		m3.next = m4;
+//		m4.next = null;		
+//		
+//		Monkey head = new Monkey(0, "Ê¯ºï");
+//		head.next = m1;
+		
+//		Monkey tail = head.next;
+//		while(true) {
+//			if ( tail.next == null) break;
+//			tail = tail.next;
+//		}		
+//		Monkey m5 = new Monkey(105, "flow");
+//		tail.next = m5;
+//		
+//		Monkey insert = new Monkey(100, "flow");
+//		insert.next = head.next;
+//		head.next = insert;
+//		
+//		Monkey start = new Monkey();
+//		Monkey former = head;
+//		start = head.next;
+//		while (true) {
+//			if (start.id  == 102)  {
+//				former.next = start.next;				
+//				break;			
+//			}
+//			former = former.next;
+//			start = start.next; 
+//		}
+//		
+//		Monkey s = head.next;
+//		while(s != null) {
+//			System.out.println(s);
+//			s = s.next;
+//		}		 
+//		
+//		System.out.println("exit");		
+		byte b = 4;
+		System.out.print(b);
+	}	
+}
